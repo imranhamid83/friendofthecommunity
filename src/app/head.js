74b4e1df5210a364379b15708f888493bf63cdd4
Imran Hamid <imranhamid83@gmail.com>
2025-08-17 17:17:27 +0100

    content changes

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Muslims in Amersham
+      <title>Muslims in Amersham & Chalfont
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
