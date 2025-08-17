25780a7cb786a5f481880bafc116192703bc3e81
Imran Hamid <imranhamid83@gmail.com>
2025-08-17 17:27:15 +0100

    update on env var

--- src/lib/cosmos.js
+++ src/lib/cosmos.js
@@ -1,12 +1,12 @@
 import { CosmosClient } from "@azure/cosmos";
 
 
-if (!process.env.COSMOS_DB_CONNECTION_STRING) {
+if (!process.env.NEXT_PUBLIC_COSMOS_DB_CONNECTION_STRING) {
     throw new Error("Missing connection string in environment variables");
   }
 
-const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-const database = client.database(process.env.COSMOS_DB_DATABASE);
+const client = new CosmosClient(process.env.NEXT_PUBLIC_COSMOS_DB_CONNECTION_STRING);
+const database = client.database(process.env.NEXT_PUBLIC_COSMOS_DB_DATABASE);
 
-export const eventsContainer = database.container(process.env.COSMOS_DB_EVENTS_CONTAINER);
-export const speakersContainer = database.container(process.env.COSMOS_DB_SPEAKERS_CONTAINER);
+export const eventsContainer = database.container(process.env.NEXT_PUBLIC_COSMOS_DB_EVENTS_CONTAINER);
+export const speakersContainer = database.container(process.env.NEXT_PUBLIC_COSMOS_DB_SPEAKERS_CONTAINER);
