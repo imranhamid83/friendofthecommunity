a088e6e3182f3119d6af4791b1a1426814b71a07
Imran Hamid <imranhamid83@gmail.com>
2025-08-26 13:46:23 +0100

    link added to book the event

--- src/app/events/[id]/page.js
+++ src/app/events/[id]/page.js
@@ -3,7 +3,6 @@ import Link from "next/link";
 
 export default async function Page({ params }) {
   const { id } = await params; // eventId
-  console.log("id:", id);
   let event = null;
 <h2>Event Details</h2>
   try {
@@ -26,10 +25,14 @@ export default async function Page({ params }) {
       <p className="text-gray-500 mb-1">{event.date} @ {event.time}</p>
       <p className="text-gray-500 mb-4">{event.location}</p>
       <p className="mb-6">{event.description}</p>
-
       <Link href={`/events/${event.eventId}/speakers`} className="text-blue-600 hover:underline">
         View Speakers →
       </Link>
+      <br/><br/>
+      <h3 className="text-xl font-bold mb-2">How to attend</h3>
+      <Link href={event.linktoattend} className="text-blue-600 hover:underline" target="_blank">
+        Book your place now → 
+      </Link>         
     </div>
   );
 }
